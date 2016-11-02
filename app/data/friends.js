2d581a133a924353b7381174d1cb1f7d40be6fa8
Monkman06 <ptkcpt2012@gmail.com>
2016-11-02 16:34:11 -0400

    completed draft

---
+++ app/data/friends.js
@@ -0,0 +1,30 @@
+var friends = [
+//set up some friends to test it out
+  {
+    name: "Neo",
+    picture: "https://upload.wikimedia.org/wikipedia/en/c/c6/NeoTheMatrix.jpg",
+    scores: [2,3,4,5,4,3,2,1,2,3],
+  },
+
+  {
+    name: "Jack",
+    picture: "https://upload.wikimedia.org/wikipedia/en/b/b9/Jack_Bauer.jpg",
+    scores: [1,2,3,2,1,3,4,5,4,3],
+  },
+
+  {
+    name: "Shaw",
+    picture: "http://vignette2.wikia.nocookie.net/pediaofinterest/images/4/49/ShawS4.jpg/revision/latest?cb=20140913134129",
+    scores: [2,1,1,1,2,5,4,4,2,1],
+  },
+  {
+    name: "Gibbs",
+    picture: "https://pmctvline2.files.wordpress.com/2014/01/104088_d0418bc.jpg",
+    scores: [2,2,2,2,1,1,2,1,4,3],
+  }
+
+];
+
+
+
+module.exports = friends;
